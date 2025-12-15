ac998e3ef21328432595c6c6b2df96c96f5cc904
motoric-o <rico.dharmawan198@gmail.com>
2025-12-15 16:15:00 +0700

    Added backup support(spatie laravel backup) - WIP

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Clean up old backups daily at 01:00
+Schedule::command('backup:clean')->dailyAt('01:00');
+
+// Run new backup daily at 01:30
+Schedule::command('backup:run')->dailyAt('01:30');
